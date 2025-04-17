419afb45f3e7466c9aed3b0bae04f29dbd504761
shaAnder <shaunanderton1@gmail.com>
2025-04-17 22:17:20 +0100

    create test category

--- server/models.py
+++ server/models.py
@@ -13,6 +13,10 @@ class ServerCategory(models.Model):
         default='default_server_uxlg3a.jpg'
       )
   
+  # for auditing
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
   def __str__(self):
     return self.name
 
@@ -30,4 +34,44 @@ class Server(models.Model):
         folder='ServerIcons',  
         default='default_server_uxlg3a.jpg'
       )
+  
+      # for auditing
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
+
+class Channel(models.Model):
+    # we want to get our choices for the chnnel
+    text = 'text'
+    voice = 'voice'
+
+    CHANNEL_TYPE_CHOICES = [
+        (text, 'Text'),
+        (voice, 'Voice'),
+    ]
+
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="owned_channels")
+    type = models.CharField(
+        max_length=5,
+        choices=CHANNEL_TYPE_CHOICES,
+        default=text,
+    )
+
+    # for now delete the channel if the server is deleted
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="server_channel")
+    # channel description, only applicable if channel is a text channel
+    description = models.CharField(max_length=255, null=True, blank=True)
+    # for auditing
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+      # Null it out if it's a voice channel, as they don't have one
+      if self.type == self.voice:
+          self.description = None  
+      self.name = self.name.lower()
+      super(Channel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
