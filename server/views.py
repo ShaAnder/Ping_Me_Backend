0fed51bf16c7508278196346b42cc7536b891d0a
shaAnder <shaunanderton1@gmail.com>
2025-04-24 15:27:47 +0100

    add auth for viewset

--- server/views.py
+++ server/views.py
@@ -2,6 +2,7 @@ from django.db.models import Count
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Server
@@ -16,6 +17,7 @@ from .serializers import ServerSerializer
 class ServerListViewSet(viewsets.ViewSet):
 
   queryset = Server.objects.all()
+  permission_classes = [IsAuthenticated]
 
   @server_list_docs
   def list(self, request):
