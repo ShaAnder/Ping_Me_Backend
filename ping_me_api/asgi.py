155c41718f1e65bfa4c5b37bb54b9f48693edbe8
shaAnder <shaunanderton1@gmail.com>
2025-05-07 15:59:18 +0100

    add auth to router

--- ping_me_api/asgi.py
+++ ping_me_api/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -22,6 +23,6 @@ from . import urls  # noqa isort:skip
 application = ProtocolTypeRouter(
     {
         "http": django_application,
-        "websocket": (URLRouter(urls.websocket_urlpatterns)),
+        "websocket": AuthMiddlewareStack(URLRouter(urls.websocket_urlpatterns)),
     }
 )
