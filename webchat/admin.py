b579eb99e71a7e9156b068b324de0360e8a21246
ShaAnder <shaunanderton1@gmail.com>
2025-05-09 19:13:10 +0100

    add new models to admin panel

--- webchat/admin.py
+++ webchat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ConversationModel, Messages
+
+admin.site.register(ConversationModel)
+admin.site.register(Messages)
