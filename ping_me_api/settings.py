959268f34b1baaced5cce9d67ff43274b11c5514
ShaAnder <shaunanderton1@gmail.com>
2025-05-10 19:16:58 +0100

    fix timestamp error in consumer

--- ping_me_api/settings.py
+++ ping_me_api/settings.py
@@ -169,7 +169,6 @@ if "DEV" in os.environ:
     }
 else:
     DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
-    print(connected)
 
 # LOGGING = {
 #     'version': 1,
