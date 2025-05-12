134359775e9e8ff192b9abc197d1b487ac773877
ShaAnder <shaunanderton1@gmail.com>
2025-05-12 17:10:32 +0100

    fix scheme error when trying to get docs

--- account/views.py
+++ account/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.response import Response
 
 from .models import Account
-from .serializers import AccountSerializer
 from .schemas import account_list_docs
+from .serializers import AccountSerializer
 
 
 class AccountViewSet(viewsets.ViewSet):
     queryset = Account.objects.all()
 
     @account_list_docs
-    def list(self, request):
-
-
-        user_id = request.query_params.get("user_id")
-        queryset = self.queryset.filter(id=user_id)
-        serializer = AccountSerializer(queryset)
-        return Response(serializer.data)
+    def list(self, request, *args, **kwargs):
+        user_id = request.query_params.get('user')
+        if user_id:
+            account = get_object_or_404(Account, user__id=user_id)
+            serializer = AccountSerializer(account)
+            return Response(serializer.data)
+        return Response({"detail": "User ID is required."}, status=400)
