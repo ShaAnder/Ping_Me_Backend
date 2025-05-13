61b97acdba8b84de50c52d8b39a7bfbfc002acc0
ShaAnder <shaunanderton1@gmail.com>
2025-05-14 00:00:43 +0100

    upgrade access token to http cookies

--- account/views.py
+++ account/views.py
@@ -30,6 +30,9 @@ class SetCookieMixin:
     def finalize_response(self, request, response, *args, **kwargs):
         if response.data.get("refresh"):
             response.set_cookie(SIMPLE_JWT["REFRESH_TOKEN_NAME"], response.data["refresh"], max_age=SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"], httponly=True, samesite=SIMPLE_JWT["JWT_COOKIE_SAMESITE"])
+        
+        if response.data.get("access"):
+            response.set_cookie(SIMPLE_JWT["ACCESS_TOKEN_NAME"], response.data["access"], max_age=SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"], httponly=True, samesite=SIMPLE_JWT["JWT_COOKIE_SAMESITE"])
         return super().finalize_response(request, response, *args, **kwargs)
     
 class CookieTokenObtainPairView(SetCookieMixin, TokenObtainPairView):
