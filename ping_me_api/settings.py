1a0c23845a05644194c03889434af457abb9c512
ShaAnder <shaunanderton1@gmail.com>
2025-05-14 11:14:46 +0100

    add custom authentication class

--- ping_me_api/settings.py
+++ ping_me_api/settings.py
@@ -234,7 +234,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # "rest_framework.authentication.SessionAuthentication",
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        "account.authenticate.JWTCookieAuthentication",
+        "account.authentication.JWTCookieAuthentication",
     ],
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
 }
