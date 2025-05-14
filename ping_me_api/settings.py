3c5400b229aa86358afd8a3f06d61054e22540dd
ShaAnder <shaunanderton1@gmail.com>
2025-05-14 17:44:27 +0100

    add access token

--- ping_me_api/settings.py
+++ ping_me_api/settings.py
@@ -248,7 +248,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     #JWTCOOKIES
     "ACCESS_TOKEN_NAME": "access_token",
