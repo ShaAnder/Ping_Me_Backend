19329faaa7c1e051fc352aba99c365ada95cebe0
ShaAnder <shaunanderton1@gmail.com>
2025-05-17 13:51:25 +0100

    fix disconnect bug

--- webchat/consumer.py
+++ webchat/consumer.py
@@ -72,5 +72,8 @@ class ChatConsumer(JsonWebsocketConsumer):
         })
 
     def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+        if hasattr(self, "room_group_name"):
+            async_to_sync(self.channel_layer.group_discard)(
+                self.room_group_name, self.channel_name
+            )
         super().disconnect(close_code)
