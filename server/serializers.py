9eccd9ed17cc5fbaa01ec1912d17c68cd351440e
ShaAnder <shaunanderton1@gmail.com>
2025-05-17 21:11:34 +0100

    fix serializer to prevent owner requirement

--- server/serializers.py
+++ server/serializers.py
@@ -10,6 +10,7 @@ class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
         fields = "__all__"
+        read_only_fields = ["owner"]
 
 
 class ServerSerializer(serializers.ModelSerializer):
