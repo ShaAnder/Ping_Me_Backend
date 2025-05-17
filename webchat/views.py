19e764a5df4636a16563827053a57226624a59ed
ShaAnder <shaunanderton1@gmail.com>
2025-05-17 16:28:29 +0100

    updated viewset for crud messages

--- webchat/views.py
+++ webchat/views.py
@@ -1,40 +1,55 @@
-from django.shortcuts import render
-from rest_framework import status, viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions, status, viewsets
 from rest_framework.response import Response
 
-from .models import ConversationModel
-from .schemas import list_message_docs
+from .models import ConversationModel, Messages
 from .serializers import MessageSerializer
 
 
+class IsSenderOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow the sender of a message to edit or delete it.
+    """
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS or obj.sender == request.user
+
 class MessageViewSet(viewsets.ViewSet):
-    @list_message_docs
+    permission_classes = [permissions.IsAuthenticated, IsSenderOrReadOnly]
+
     def list(self, request):
         channel_id = request.query_params.get("channel_id")
-        try:
-            conversation = ConversationModel.objects.filter(channel_id=channel_id).first()
-            message = conversation.messages.all()
-            serializer = MessageSerializer(message, many=True)
+        conversation = ConversationModel.objects.filter(channel_id=channel_id).first()
+        if conversation:
+            messages = conversation.messages.all()
+            serializer = MessageSerializer(messages, many=True)
             return Response(serializer.data)
-        except ConversationModel.DoesNotExist:
-            return Response([])
-        
+        return Response([])
 
-# class LastChannelViewSet(viewsets.ViewSet):
-#     permission_classes = [IsAuthenticated]
+    def retrieve(self, request, pk=None):
+        try:
+            msg = Messages.objects.get(pk=pk)
+        except Messages.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        self.check_object_permissions(request, msg)
+        serializer = MessageSerializer(msg)
+        return Response(serializer.data)
 
-#     # GET /api/servers/{server_id}/last-channel/
-#     def list(self, request, server_pk=None):
-#         last = LastChannel.objects.filter(user=request.user, server_id=server_pk).first()
-#         return Response({"channel_id": last.channel_id if last else None})
+    def partial_update(self, request, pk=None):
+        try:
+            msg = Messages.objects.get(pk=pk)
+        except Messages.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        self.check_object_permissions(request, msg)
+        serializer = MessageSerializer(msg, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     # POST /api/servers/{server_id}/last-channel/  { "channel_id": "abc123" }
-#     def create(self, request, server_pk=None):
-#         cid = request.data.get("channel_id")
-#         obj, _ = LastChannel.objects.update_or_create(
-#             user=request.user,
-#             server_id=server_pk,
-#             defaults={"channel_id": cid},
-#         )
-#         return Response({"channel_id": obj.channel_id}, status=status.HTTP_201_CREATED)
+    def delete(self, request, pk=None):
+        try:
+            msg = Messages.objects.get(pk=pk)
+        except Messages.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        self.check_object_permissions(request, msg)
+        msg.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
