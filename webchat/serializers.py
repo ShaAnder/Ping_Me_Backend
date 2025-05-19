9c1f833fb2ef15d93b44d7c7a88d9af7cacc8e37
ShaAnder <shaunanderton1@gmail.com>
2025-05-19 12:59:43 +0100

    update message serializer to tie to account

--- webchat/serializers.py
+++ webchat/serializers.py
@@ -1,32 +1,19 @@
 from rest_framework import serializers
 
+from account.serializers import AccountSerializer
+
 from .models import Messages
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender_username = serializers.SerializerMethodField()
-    sender_avatar_url = serializers.SerializerMethodField()
+    user = AccountSerializer(source='sender.account', read_only=True)
 
     class Meta:
         model = Messages
         fields = [
             "id",
-            "sender_username",      # display name
-            "sender_avatar_url",    # avatar url
+            "user",
             "content",
             "timestamp_created",
             "timestamp_updated",
         ]
-
-    def get_sender_username(self, obj):
-        # Get the Account model's username (display name)
-        account = getattr(obj.sender, "account", None)
-        if account and account.username:
-            return account.username
-        return obj.sender.username  # fallback to User.username
-
-    def get_sender_avatar_url(self, obj):
-        account = getattr(obj.sender, "account", None)
-        if account and account.image:
-            return account.image.url
-        return None
