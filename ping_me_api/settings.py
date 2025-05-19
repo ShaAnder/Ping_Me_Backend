802f28e053cb997cac18da54f5e305c0c5400a70
ShaAnder <shaunanderton1@gmail.com>
2025-05-19 08:35:37 +0100

    edit token lifetimes for prog

--- ping_me_api/settings.py
+++ ping_me_api/settings.py
@@ -241,8 +241,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=4),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
